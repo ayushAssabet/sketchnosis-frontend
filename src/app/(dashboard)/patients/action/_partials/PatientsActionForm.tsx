1e579bcb01b3fb54ed03d0258726ac050e1feff8
subash-assabet <subash@ubventuresllc.com>
2025-08-13 14:53:18 +0545

    fix: patient add

--- src/app/(dashboard)/patients/action/_partials/PatientsActionForm.tsx
+++ src/app/(dashboard)/patients/action/_partials/PatientsActionForm.tsx
@@ -9,7 +9,6 @@ import { usePatient } from "@/features/patients/usePatientAction";
 import { usePatientActionForm } from "@/features/patients/usePatientActionForm";
 import { useGetPatientDetail } from "@/features/patients/useGetPatient";
 import { appRoutes } from "@/lib/routes";
-import { Loader } from "lucide-react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { FormEvent, useEffect, useMemo } from "react";
@@ -17,272 +16,274 @@ import { Label } from "@/components/ui/label";
 import CampaignSelector from "@/components/elements/SelectCampaignDialog";
 
 const PatientActionForm: React.FC<{
-    isUpdate: boolean;
+  isUpdate: boolean;
 }> = ({ isUpdate }) => {
-    const searchParams = useSearchParams();
-    const patientId = searchParams.get("update");
+  const searchParams = useSearchParams();
+  const patientId = searchParams.get("update");
 
-    const { formData, handleChange, errors, setFormData, validateForm } =
-        usePatientActionForm();
+  const { formData, handleChange, errors, setFormData, validateForm } =
+    usePatientActionForm();
 
-    const { addPatient, updatePatient, isAddingPatient, isUpdatingPatient } =
-        usePatient();
+  const { addPatient, updatePatient, isAddingPatient, isUpdatingPatient } =
+    usePatient();
 
-    const { data, isLoading } = useGetPatientDetail(
-        searchParams?.get("update")
-    );
+  const { data, isLoading } = useGetPatientDetail(searchParams?.get("update"));
 
-    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const result = validateForm();
-        if (!result.success) return;
-        isUpdate ? updatePatient(patientId!, formData!) : addPatient(formData!);
-    };
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const result = validateForm();
+    if (!result.success) return;
+    isUpdate ? updatePatient(patientId!, formData!) : addPatient(formData!);
+  };
 
-    const defaultCategories = useMemo(() => {
-        return (
-            data?.data?.areaOfConcerns?.map(
-                (category: Record<string, any>) => ({
-                    label: category?.name,
-                    value: category?.id,
-                })
-            ) ?? []
-        );
-    }, [data]);
-
-    const handleAreaOfConcernsChange = (
-        selected: { label: string; value: string }[]
-    ) => {
-        console.log(selected);
-        setFormData((prev) => ({
-            ...prev,
-            areaOfConcernIds: selected.map((item) => item.value),
-        }));
-    };
-
-    const handleGenderChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const value = e.target.value as unknown as "male" | "female" | "other";
-        setFormData((prev) => ({
-            ...prev,
-            gender: value,
-        }));
-    };
-
-    const handleCampaignSelect = (campaign: any, startDate: string) => {
-        setFormData((prev) => ({
-            ...prev,
-            campaignId: campaign?.id || null,
-            campaignStartDate: startDate || null,
-        }));
-    };
-
-    useEffect(() => {
-        if (data?.data && patientId) {
-            setFormData({
-                firstName: data?.data?.firstName,
-                lastName: data?.data?.lastName,
-                email: data?.data?.email,
-                phone: data?.data?.phone,
-                address: data?.data?.address,
-                areaOfConcernIds: data?.data?.areaOfConcerns?.map(
-                    (concern: Record<string, any>) => concern.id
-                ),
-                dob: data?.data?.dob,
-                gender: data?.data?.gender,
-                campaignId: data?.data?.campaignId,
-                campaignStartDate: data?.data?.campaignStartDate,
-                description: data?.data?.description,
-            });
-        }
-    }, [data, setFormData]);
-
-    console.log(formData);
-
+  const defaultCategories = useMemo(() => {
     return (
-        <>
-            <form
-                className="grid grid-cols-2 gap-x-20 gap-y-8 py-6"
-                onSubmit={handleSubmit}
-            >
-                <AppInputField
-                    id="firstName"
-                    name="firstName"
-                    label="First Name"
-                    className="text-sm"
-                    type="text"
-                    value={formData?.firstName ?? ""}
-                    onChange={handleChange}
-                    placeholder="Eg: John"
-                    error={errors?.firstName}
-                    disabled={isAddingPatient || isUpdatingPatient}
-                    variant="dashboard"
-                    required
-                />
-
-                <AppInputField
-                    id="lastName"
-                    name="lastName"
-                    label="Last Name"
-                    className="text-sm"
-                    type="text"
-                    value={formData?.lastName ?? ""}
-                    onChange={handleChange}
-                    placeholder="Eg: Doe"
-                    error={errors?.lastName}
-                    disabled={isAddingPatient || isUpdatingPatient}
-                    variant="dashboard"
-                    required
-                />
-
-                <AppInputField
-                    id="email"
-                    name="email"
-                    className="text-sm"
-                    label="Email Address"
-                    type="email"
-                    value={formData?.email ?? ""}
-                    onChange={handleChange}
-                    placeholder="Eg: john.doe@example.com"
-                    error={errors?.email}
-                    disabled={isAddingPatient || isUpdatingPatient}
-                    variant="dashboard"
-                    required
-                    readonly={isUpdate}
-                />
-
-                <AppInputField
-                    id="phone"
-                    name="phone"
-                    className="text-sm"
-                    label="Phone Number"
-                    type="tel"
-                    value={formData?.phone ?? ""}
-                    onChange={handleChange}
-                    placeholder="Eg: 9745716809"
-                    error={errors?.phone}
-                    disabled={isAddingPatient || isUpdatingPatient}
-                    variant="dashboard"
-                    required
-                    readonly={isUpdate}
-                />
-
-                <AppInputField
-                    id="address"
-                    name="address"
-                    className="text-sm"
-                    label="Address"
-                    type="text"
-                    value={formData?.address ?? ""}
-                    onChange={handleChange}
-                    placeholder="Eg: 13th Street"
-                    error={errors?.address}
-                    disabled={isAddingPatient || isUpdatingPatient}
-                    variant="dashboard"
-                />
-
-                <AppInputField
-                    id="dob"
-                    name="dob"
-                    className="text-sm"
-                    label="Date of Birth"
-                    type="date"
-                    value={formData?.dob ?? ""}
-                    onChange={handleChange}
-                    error={errors?.dob}
-                    disabled={isAddingPatient || isUpdatingPatient}
-                    variant="dashboard"
-                    required
-                />
-
-                <div className="">
-                    <Label className="block text-medium font-medium text-gray-700 mb-2 pt-1">
-                        Gender
-                        <span className="text-red-500">*</span>
-                    </Label>
-                    <select
-                        id="gender"
-                        name="gender"
-                        value={formData?.gender ?? ""}
-                        onChange={handleGenderChange}
-                        className="px-3 py-2 h-[42px] border w-full border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-sm"
-                        disabled={isAddingPatient || isUpdatingPatient}
-                        required
-                    >
-                        <option value="">Select Gender</option>
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                        <option value="other">Other</option>
-                    </select>
-                    {errors?.gender && (
-                        <span className="text-red-500 text-xs">
-                            {errors.gender}
-                        </span>
-                    )}
-                </div>
-
-                <AsyncSearchableDropdown
-                    defaultOption={defaultCategories}
-                    onSelectionChange={handleAreaOfConcernsChange}
-                    error={errors?.areaOfConcernIds}
-                    required
-                />
-
-                <CampaignSelector
-                    selectedCampaign={
-                        data?.data?.campaign || formData?.campaignId
-                    }
-                    selectedStartDate={formData?.campaignStartDate ?? ""}
-                    onCampaignSelect={handleCampaignSelect}
-                    disabled={isAddingPatient || isUpdatingPatient}
-                    showStartDate={true}
-                />
-                <AppInputField
-                    id="campaignStartDate"
-                    name="campaignStartDate"
-                    className="text-sm"
-                    label="Campaign Start Date"
-                    type="date"
-                    value={formData?.campaignStartDate ?? ""}
-                    onChange={handleChange}
-                    error={errors?.campaignStartDate}
-                    disabled={isAddingPatient || isUpdatingPatient}
-                    variant="dashboard"
-                />
-
-                <div className="col-span-2">
-                    <AppTextArea
-                        id="description"
-                        name="description"
-                        className="text-sm"
-                        label="Description/Notes"
-                        value={formData?.description ?? ""}
-                        onChange={handleChange}
-                        placeholder="Eg: Additional patient information or medical notes"
-                        error={errors?.description}
-                        disabled={isAddingPatient || isUpdatingPatient}
-                        variant="dashboard"
-                    />
-                </div>
-
-                <div></div>
-
-                <div className="">
-                    <div className="gap-5 grid grid-cols-2">
-                        <Link href={appRoutes.PATIENT_INDEX_PAGE}>
-                            <Button variant="outline" className="h-full w-full">
-                                Cancel
-                            </Button>
-                        </Link>
-                        <LoadingButton
-                            title="Patient"
-                            isLoading={isAddingPatient || isUpdatingPatient}
-                            isUpdate={isUpdate}
-                        />
-                    </div>
-                </div>
-            </form>
-        </>
+      data?.data?.areaOfConcerns?.map((category: Record<string, any>) => ({
+        label: category?.name,
+        value: category?.id,
+      })) ?? []
     );
+  }, [data]);
+
+  const handleAreaOfConcernsChange = (
+    selected: { label: string; value: string }[]
+  ) => {
+    console.log(selected);
+    setFormData((prev) => ({
+      ...prev,
+      areaOfConcernIds: selected.map((item) => item.value),
+    }));
+  };
+
+  const handleGenderChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value as unknown as "male" | "female" | "other";
+    setFormData((prev) => ({
+      ...prev,
+      gender: value,
+    }));
+  };
+
+  const handleCampaignSelect = (campaign: any, startDate?: string) => {
+    setFormData((prev) => ({
+      ...prev,
+      campaigns: [
+        {
+          id: campaign?.id || null,
+          startDate: startDate || null,
+        },
+      ],
+    }));
+  };
+
+  useEffect(() => {
+    if (data?.data && patientId) {
+      setFormData({
+        firstName: data?.data?.firstName,
+        lastName: data?.data?.lastName,
+        email: data?.data?.email,
+        phone: data?.data?.phone,
+        address: data?.data?.address,
+        areaOfConcernIds: data?.data?.areaOfConcerns?.map(
+          (concern: Record<string, any>) => concern.id
+        ),
+        dob: data?.data?.dob,
+        gender: data?.data?.gender,
+        campaigns: [
+          {
+            id: data?.data?.campaignId,
+            startDate: data?.data?.campaignStartDate,
+          },
+        ],
+        description: data?.data?.description,
+      });
+    }
+  }, [data, setFormData]);
+
+  console.log(formData);
+
+  return (
+    <>
+      <form
+        className="grid grid-cols-2 gap-x-20 gap-y-8 py-6"
+        onSubmit={handleSubmit}
+      >
+        <AppInputField
+          id="firstName"
+          name="firstName"
+          label="First Name"
+          className="text-sm"
+          type="text"
+          value={formData?.firstName ?? ""}
+          onChange={handleChange}
+          placeholder="Eg: John"
+          error={errors?.firstName}
+          disabled={isAddingPatient || isUpdatingPatient}
+          variant="dashboard"
+          required
+        />
+
+        <AppInputField
+          id="lastName"
+          name="lastName"
+          label="Last Name"
+          className="text-sm"
+          type="text"
+          value={formData?.lastName ?? ""}
+          onChange={handleChange}
+          placeholder="Eg: Doe"
+          error={errors?.lastName}
+          disabled={isAddingPatient || isUpdatingPatient}
+          variant="dashboard"
+          required
+        />
+
+        <AppInputField
+          id="email"
+          name="email"
+          className="text-sm"
+          label="Email Address"
+          type="email"
+          value={formData?.email ?? ""}
+          onChange={handleChange}
+          placeholder="Eg: john.doe@example.com"
+          error={errors?.email}
+          disabled={isAddingPatient || isUpdatingPatient}
+          variant="dashboard"
+          required
+          readonly={isUpdate}
+        />
+
+        <AppInputField
+          id="phone"
+          name="phone"
+          className="text-sm"
+          label="Phone Number"
+          type="tel"
+          value={formData?.phone ?? ""}
+          onChange={handleChange}
+          placeholder="Eg: 9745716809"
+          error={errors?.phone}
+          disabled={isAddingPatient || isUpdatingPatient}
+          variant="dashboard"
+          required
+          readonly={isUpdate}
+        />
+
+        <AppInputField
+          id="address"
+          name="address"
+          className="text-sm"
+          label="Address"
+          type="text"
+          value={formData?.address ?? ""}
+          onChange={handleChange}
+          placeholder="Eg: 13th Street"
+          error={errors?.address}
+          disabled={isAddingPatient || isUpdatingPatient}
+          variant="dashboard"
+        />
+
+        <AppInputField
+          id="dob"
+          name="dob"
+          className="text-sm"
+          label="Date of Birth"
+          type="date"
+          value={formData?.dob ?? ""}
+          onChange={handleChange}
+          error={errors?.dob}
+          disabled={isAddingPatient || isUpdatingPatient}
+          variant="dashboard"
+          required
+        />
+
+        <div className="">
+          <Label className="block text-medium font-medium text-gray-700 mb-2 pt-1">
+            Gender
+            <span className="text-red-500">*</span>
+          </Label>
+          <select
+            id="gender"
+            name="gender"
+            value={formData?.gender ?? ""}
+            onChange={handleGenderChange}
+            className="px-3 py-2 h-[42px] border w-full border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-sm"
+            disabled={isAddingPatient || isUpdatingPatient}
+            required
+          >
+            <option value="">Select Gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+          {errors?.gender && (
+            <span className="text-red-500 text-xs">{errors.gender}</span>
+          )}
+        </div>
+
+        <AsyncSearchableDropdown
+          defaultOption={defaultCategories}
+          onSelectionChange={handleAreaOfConcernsChange}
+          error={errors?.areaOfConcernIds}
+          required
+        />
+
+        <CampaignSelector
+          selectedCampaign={
+            (data?.data?.campaign || formData?.campaigns[0]?.id) ?? ""
+          }
+          selectedStartDate={formData?.campaigns[0]?.startDate ?? ""}
+          onCampaignSelect={handleCampaignSelect}
+          disabled={isAddingPatient || isUpdatingPatient}
+          showStartDate={true}
+        />
+        <AppInputField
+          id="campaignStartDate"
+          name="campaignStartDate"
+          className="text-sm"
+          label="Campaign Start Date"
+          type="date"
+          value={formData?.campaigns[0]?.startDate ?? ""}
+          onChange={handleChange}
+          error={errors?.campaignStartDate}
+          disabled={isAddingPatient || isUpdatingPatient}
+          variant="dashboard"
+        />
+
+        <div className="col-span-2">
+          <AppTextArea
+            id="description"
+            name="description"
+            className="text-sm"
+            label="Description/Notes"
+            value={formData?.description ?? ""}
+            onChange={handleChange}
+            placeholder="Eg: Additional patient information or medical notes"
+            error={errors?.description}
+            disabled={isAddingPatient || isUpdatingPatient}
+            variant="dashboard"
+          />
+        </div>
+
+        <div></div>
+
+        <div className="">
+          <div className="gap-5 grid grid-cols-2">
+            <Link href={appRoutes.PATIENT_INDEX_PAGE}>
+              <Button variant="outline" className="h-full w-full">
+                Cancel
+              </Button>
+            </Link>
+            <LoadingButton
+              title="Patient"
+              isLoading={isAddingPatient || isUpdatingPatient}
+              isUpdate={isUpdate}
+            />
+          </div>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default PatientActionForm;
--- src/features/patients/usePatientActionForm.tsx
+++ src/features/patients/usePatientActionForm.tsx
@@ -3,59 +3,58 @@ import { ZodError } from "zod";
 import { PatientFormData, patientSchema } from "./patient.schema";
 
 export const usePatientActionForm = (data?: PatientFormData | null) => {
-    const [formData, setFormData] = useState<Partial<PatientFormData>>({
-        firstName: "",
-        lastName: "",
-        email: "",
-        phone: "",
-        address: "",
-        areaOfConcernIds: [],
-        dob: "",
-        gender: null,
-        campaignId: null,
-        campaignStartDate: null,
-        description: "",
-    });
+  const [formData, setFormData] = useState<Partial<PatientFormData>>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    address: "",
+    areaOfConcernIds: [],
+    dob: "",
+    gender: null,
+    campaigns: [],
+    description: "",
+  });
 
-    const [errors, setErrors] = useState<Record<string, any>>({});
+  const [errors, setErrors] = useState<Record<string, any>>({});
 
-    useEffect(() => {
-        if(data) setFormData(data)
-    },[data])
+  useEffect(() => {
+    if (data) setFormData(data);
+  }, [data]);
 
-    const handleChange = (
-        e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-    ) => {
-        const { value, id } = e.currentTarget;
-        setFormData((prev) => ({
-            ...prev,
-            [id]: id == "phone" ? value.toString() : value,
-        }));
-    };
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { value, id } = e.currentTarget;
+    setFormData((prev) => ({
+      ...prev,
+      [id]: id == "phone" ? value.toString() : value,
+    }));
+  };
 
-    const validateForm = () => {
-        try {
-            const validated = patientSchema.parse(formData);
-            setErrors({});
-            return { success: true, data: validated };
-        } catch (err) {
-            if (err instanceof ZodError) {
-                const formatted: Record<string, string> = {};
-                err.errors.forEach((e) => {
-                    if (e.path[0]) formatted[e.path[0] as string] = e.message;
-                });
-                setErrors(formatted);
-            }
-            return { success: false };
-        }
-    };
+  const validateForm = () => {
+    try {
+      const validated = patientSchema.parse(formData);
+      setErrors({});
+      return { success: true, data: validated };
+    } catch (err) {
+      if (err instanceof ZodError) {
+        const formatted: Record<string, string> = {};
+        err.errors.forEach((e) => {
+          if (e.path[0]) formatted[e.path[0] as string] = e.message;
+        });
+        setErrors(formatted);
+      }
+      return { success: false };
+    }
+  };
 
-    return {
-        formData,
-        setFormData,
-        handleChange,
-        errors,
-        setErrors,
-        validateForm,
-    };
+  return {
+    formData,
+    setFormData,
+    handleChange,
+    errors,
+    setErrors,
+    validateForm,
+  };
 };
